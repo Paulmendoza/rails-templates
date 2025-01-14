948e964b01f26804b16d3058226fb4d73da36401
Paul Mendoza <pjamendoza@proton.me>
2025-01-13 23:10:42 -0800

    reorg. add nav

--- template-base.rb
+++ template-base.rb
@@ -1,56 +1,36 @@
+# Devise
 gem 'devise'
 run 'bundle install'
 generate "devise:install"
 generate "devise:views"
+generate "devise", "user"
+
+# Omniauth
 gem 'omniauth'
 gem 'omniauth-rails_csrf_protection'
 gem 'omniauth-facebook'
 gem 'omniauth-google-oauth2'
 run 'bundle install'
-generate "devise", "user"
+
 inject_into_file "config/routes.rb", after: "devise_for :users" do <<-RUBY
-, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-RUBY
+  , controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  RUBY
 end
+
+# Integrate Omniauth with Devise users
 inject_into_file "app/models/user.rb", before: "end" do <<-RUBY
   devise :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
-
+  
   def self.from_omniauth(access_token)
     user = User.where(email: access_token.info.email).first
-
+    
     user ||= User.create!(provider: access_token.provider, uid: access_token.uid, email: access_token.info.email, password: Devise.friendly_token[0, 20])
     user
   end
-RUBY
+  RUBY
 end
-
-generate :migration, "AddOmniauthToUsers", "provider:string", "uid:string"
-rails_command 'db:migrate'
-
-inject_into_file "config/initializers/devise.rb", after: "# ==> OmniAuth\n" do <<-CODE
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], {scope: 'email'}
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: 'email', prompt: 'select_account'}
-CODE
-end
-
-inject_into_file "app/views/layouts/application.html.erb", after: "<body>\n" do <<-CODE
-  <p class="notice"><%= notice %></p>
-  <p class="alert"><%= alert %></p>
-CODE
-end
-
-generate :controller, "home", "index"
-
-append_file  "app/views/home/index.html.erb", <<-CODE
-<%= button_to "Register with Facebook", user_facebook_omniauth_authorize_path, method: :post, data: { turbo: 'false' }  %>
-<%= button_to "Register with Google", user_google_oauth2_omniauth_authorize_path, method: :post, data: { turbo: 'false' } %>
-CODE
-
-route "root to: 'home#index'"
 
 generate "devise:controllers", "users", "-c=omniauth_callbacks"
-
-route "devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }"
 
 inject_into_file "app/controllers/users/omniauth_callbacks_controller.rb", before: "\n  # You should also create an action method in this controller like this:" do <<-CODE
   def google_oauth2
@@ -81,6 +61,33 @@ inject_into_file "app/controllers/users/omniauth_callbacks_controller.rb", befor
   end
 CODE
 end
+
+route "devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }"
+
+generate :migration, "AddOmniauthToUsers", "provider:string", "uid:string"
+rails_command 'db:migrate'
+
+# Config Omniauth
+inject_into_file "config/initializers/devise.rb", after: "# ==> OmniAuth\n" do <<-CODE
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], {scope: 'email'}
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: 'email', prompt: 'select_account'}
+CODE
+end
+
+inject_into_file "app/views/layouts/application.html.erb", after: "<body>\n" do <<-CODE
+  <nav>
+    <%= link_to "Home", root_path %>
+    <%= link_to "Sign In", new_user_session_path %>
+    <%= link_to "Sign Up", new_user_registration_path %>
+  </nav>
+  <p class="notice"><%= notice %></p>
+  <p class="alert"><%= alert %></p>
+CODE
+end
+
+generate :controller, "home", "index"
+
+route "root to: 'home#index'"
 
 file ".devcontainer/devcontainer.json", <<-JSON
   {
